cec1dbcc2b79802b06ba78ff2d54e279cd27b179
Samuel <samuelalves681@gmail>
2022-07-27 21:07:19 -0300

    responsive header

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,12 +1,18 @@
 import { Avatar, Box, Flex, Text } from "@chakra-ui/react";
 
-export function Profile() {
+interface InterfaceProfileProps {
+  showProfileData: boolean;
+}
+
+export function Profile({ showProfileData = true }: InterfaceProfileProps) {
   return (
     <Flex align="center">
-      <Box mr="4" textAlign="right">
-        <Text>Samuel Alves</Text>
-        <Text color="gray.300">samuelalves681@gmail.com</Text>
-      </Box>
+      {showProfileData && (
+        <Box mr="4" textAlign="right">
+          <Text>Samuel Alves</Text>
+          <Text color="gray.300">samuelalves681@gmail.com</Text>
+        </Box>
+      )}
       <Avatar
         size="md"
         name="Samuel Alves"
